fb8c7544343b4456dcb72695bce260308d7273a6
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-23 23:50:31 +0000

    Add split documentation

--- samdown/raw.py
+++ samdown/raw.py
@@ -1,2 +1,9 @@
+"""Functions for processing raw markdown."""
+
 def split(raw_text):
+    """Takes a chunk of raw markdown and splits it into blocks.
+
+    :param str raw_text: The raw markdown.
+    :rtype: ``list``"""
+
     return raw_text.replace("\r\n", "\n").split("\n\n")
