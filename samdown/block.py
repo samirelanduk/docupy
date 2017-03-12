847b481c8b2e671b91b4de0ab2b735142b1fa0d4
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-12 21:11:39 +0000

    Add underline processing

--- samdown/block.py
+++ samdown/block.py
@@ -1,7 +1,10 @@
 """Functions for turning markdown blocks into HTML elements."""
 
+import re
+
 def process_block(block):
     """Takes a block of Markdown and returns the correct HTML for that
     block, based on its contents."""
 
+    block = re.sub(r"_(.*?)_", r"<u>\1</u>", block)
     return "<p>%s</p>" % block
--- tests/test_block_processing.py
+++ tests/test_block_processing.py
@@ -5,3 +5,10 @@ class BasicParagraphTests(TestCase):
 
     def test_can_turn_block_into_paragraph(self):
         self.assertEqual(process_block("Text block."), "<p>Text block.</p>")
+
+
+    def test_can_underline_elements(self):
+        self.assertEqual(
+         process_block("_Text_ _block_."),
+         "<p><u>Text</u> <u>block</u>.</p>"
+        )
