5bcc928680094f5bcd3d59343e37720a56078c64
Sam Ireland <sam.ireland.uk@gmail.com>
2017-03-12 21:02:34 +0000

    Start 0.2

--- samdown/__init__.py
+++ samdown/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Sam Ireland"
 
 from .raw import html_from_markdown
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="samdown",
- version="0.1.0",
+ version="0.2.0",
  description="A markdown library",
  url="https://samdown.samireland.com",
  author="Sam Ireland",
