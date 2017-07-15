e2b333cfdc4db7a3a17f716c8eddb11609116986
Sam Ireland <sam.ireland.uk@gmail.com>
2017-07-15 21:13:45 +0100

    Update function docstrings

--- django_samdown/block.py
+++ django_samdown/block.py
@@ -1,10 +1,28 @@
-"""Functions for turning markdown blocks into HTML elements."""
+"""Functions for turning samdown blocks into HTML elements."""
 
 import re
 
 def process_block(block, lookup):
-    """Takes a block of Markdown and returns the correct HTML for that
-    block, based on its contents."""
+    """Takes a block of samdown and returns the correct HTML for that
+    block, based on its contents.
+
+    This function requires a lookup dictionary that tells it what regex patterns
+    to look for and what to replace those patterns with. This dictionary needs
+    a 'inline' subdictionary and a 'block' subdictionary. That is:
+
+    ``{
+     'inline': {
+      ...
+     }, 'block': {
+      ...
+     }
+    }``
+
+    These two sub-dictionaries should have regex expressions as keys, and regex
+    replacement expressions as values (although blocks can have functions also).
+
+    :param str block: The block of samdown to turn into HTML.
+    :param dict lookup: The lookup dictionary to translate the block with."""
 
     for pattern in lookup["block"]:
         compiled = re.compile(pattern)
--- django_samdown/raw.py
+++ django_samdown/raw.py
@@ -1,18 +1,23 @@
 from .block import process_block
 from django.conf import settings
 
-"""Functions for processing raw markdown."""
+"""Functions for processing raw samdown."""
 
 def split(raw_text):
-    """Takes a chunk of raw markdown and splits it into blocks.
+    """Takes a chunk of raw samdown and splits it into blocks.
 
-    :param str raw_text: The raw markdown.
+    :param str raw_text: The raw samdown.
     :rtype: ``list``"""
 
     return raw_text.replace("\r\n", "\n").split("\n\n")
 
 
 def get_lookup():
+    """Creates a lookup dictionary, using pre-defined rules and the samdown
+    settings in your Django application's settings.py file.
+
+    :rtype: ``dict``"""
+
     lookup = {
      "inline": {
       r"_(.*?)_": r"<u>\1</u>",
@@ -27,11 +32,10 @@ def get_lookup():
 
 
 def html_from_markdown(samdown):
-    """Converts markdown text into HTML.
+    """Converts samdown text into HTML.
 
-    :param str markdown: The markdown text to convert to HTML.
+    :param str samdown: The samdown text to convert to HTML.
     :rtype: ``str``"""
 
     lookup = get_lookup()
-
     return "\n".join([process_block(block, lookup) for block in split(samdown)])
